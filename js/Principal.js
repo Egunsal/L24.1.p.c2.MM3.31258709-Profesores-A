d633828171fb126f72b854e78825262d7fdd4991
Willian Alcon <1001.31258709.ucla@gmail.com>
2024-10-10 11:18:16 -0400

    Principal.js

--- js/Principal.js
+++ js/Principal.js
@@ -1,8 +1,10 @@
-import Cl_Contratado from "./Cl_Contratado.js";
 import Cl_Fijo from "./Cl_Fijo.js";
 
 let fijo1 = new Cl_Fijo("Carlos",25,100)
 
 
 let salida = document.getElementById("salida")
-salida.innerHTML = "Ingreso total del profesor "+fijo1.mostrarNombre()+": "+fijo1.sueldoTotal();
+salida.innerHTML ="Nombre del profesor fijo: "+fijo1.mostrarNombre();
+salida.innerHTML +="<br>Monto del bono: "+fijo1.mostrarBono();
+salida.innerHTML +="<br>Monto del sueldo: "+fijo1.mostarSueldo();
+salida.innerHTML += "<br>Ingreso total del profesor "+fijo1.mostrarNombre()+": "+fijo1.sueldoTotal();
