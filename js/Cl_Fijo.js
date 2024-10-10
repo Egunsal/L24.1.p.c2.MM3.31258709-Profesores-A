fbed991e968e69b8dddcd936197b353db87383cb
Willian Alcon <1001.31258709.ucla@gmail.com>
2024-10-10 11:18:41 -0400

    Update Cl_Fijo.js

--- js/Cl_Fijo.js
+++ js/Cl_Fijo.js
@@ -16,4 +16,10 @@ export default class Cl_Fijo extends Cl_Profesor{
     mostrarNombre(){
         return this._nombre;
     }
+    mostrarBono(){
+        return this._bono;
+    }
+    mostarSueldo(){
+        return this._sueldo;
+    }
 } 
